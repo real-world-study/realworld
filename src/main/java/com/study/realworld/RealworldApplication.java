ec68a13b37ee3f69796aed4ba701cbe73a4680e3
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-07-26 12:52:50 +0900

    Initial commit

---
+++ src/main/java/com/study/realworld/RealworldApplication.java
@@ -0,0 +1,13 @@
+package com.study.realworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealworldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/realworld/RealworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.realworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class RealworldApplicationTests {
+
+    @Test
+    void main() {
+        RealworldApplication.main(new String[] {});
+    }
+
+}
