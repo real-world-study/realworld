59b3a1dde7df15f52014101251c815f053693288
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-07-27 17:18:39 +0900

    feat: 오타수정

--- src/main/resources/schema-h2.sql
+++ src/main/resources/schema-h2.sql
@@ -42,23 +42,23 @@ CREATE TABLE comment
 (
     id              bigint          NOT NULL AUTO_INCREMENT,
     user_id         bigint          NOT NULL,
-    airticle_id     bigint          NOT NULL,
+    article_id     bigint          NOT NULL,
     body            text            NOT NULL,
     created_at      DATETIME        NOT NULL DEFAULT CURRENT_TIMESTAMP(),
     updated_at      DATETIME        DEFAULT NULL,
     deleted_at      DATETIME        DEFAULT NULL,
     PRIMARY KEY (id),
     CONSTRAINT fk_comment_to_user_id FOREIGN KEY (user_id) REFERENCES user (id),
-    CONSTRAINT fk_comment_to_airticle_id FOREIGN KEY (airticle_id) REFERENCES article (id) ON DELETE CASCADE ON UPDATE CASCADE
+    CONSTRAINT fk_comment_to_article_id FOREIGN KEY (article_id) REFERENCES article (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 CREATE TABLE favorite
 (
     user_id         bigint          NOT NULL,
-    airticle_id     bigint          NOT NULL,
-    PRIMARY KEY (user_id, airticle_id),
+    article_id     bigint          NOT NULL,
+    PRIMARY KEY (user_id, article_id),
     CONSTRAINT fk_favorite_to_user_id FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE ,
-    CONSTRAINT fk_favorite_to_airticle_id FOREIGN KEY (airticle_id) REFERENCES article (id) ON DELETE CASCADE
+    CONSTRAINT fk_favorite_to_article_id FOREIGN KEY (article_id) REFERENCES article (id) ON DELETE CASCADE
 );
 
 CREATE TABLE tag
