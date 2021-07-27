7219cf8b3e32c2bf9835aaf75863c28959e41b60
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-07-27 22:26:56 +0900

    [#1] feat: security config 추가
    - public source 권한 해제
    - encoding을 위한 PasswordEncoder bean 등록

---
+++ src/main/java/com/study/realworld/global/config/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.study.realworld.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/static/**", "/templates/**", "/h2/**", "/h2-console/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/users", "/api/users/login").permitAll()
+                .anyRequest().authenticated();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+}
