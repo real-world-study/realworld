1b0e3ad9b833374eb0b9e418150401862bc16e84
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-07-29 04:10:38 +0900

    [#1] test: encodePassword 메소드 테스트 추가

--- src/test/java/com/study/realworld/user/domain/UserTest.java
+++ src/test/java/com/study/realworld/user/domain/UserTest.java
@@ -2,10 +2,21 @@ package com.study.realworld.user.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import static java.lang.String.valueOf;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class UserTest {
+
+    @Mock
+    private PasswordEncoder passwordEncoder;
 
     @Test
     void userTest() {
@@ -65,6 +76,21 @@ class UserTest {
         assertThat(user.getPassword()).isEqualTo(input.getPassword());
         assertThat(user.getBio()).isEqualTo(input.getBio());
         assertThat(user.getImage()).isEqualTo(input.getImage());
+    }
+
+    @Test
+    @DisplayName("encodePassword 메소드가 실행되었을 때 User 객체의 비밀번호가 인코딩되어야 한다.")
+    void userEncodePasswordTest() {
+
+        // setup & given
+        User user = new User.Builder().password(new Password("password")).build();
+        when(passwordEncoder.encode(valueOf(user.getPassword()))).thenReturn("encoded_password");
+
+        // when
+        user.encodePassword(passwordEncoder);
+
+        // then
+        assertThat(user.getPassword().getPassword()).isEqualTo("encoded_password");
     }
 
     @Test
