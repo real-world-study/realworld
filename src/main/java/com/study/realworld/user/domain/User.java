baa12c76adc131b1527215283ddefdf01616c051
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-07-28 11:34:00 +0900

    [#1] feat: generate type IDENTITY 변경

--- src/main/java/com/study/realworld/user/domain/User.java
+++ src/main/java/com/study/realworld/user/domain/User.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Embedded
