4a3532356d5ec4822590545d587c37559bc8a382
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-07-28 09:57:20 +0900

    [#1] feat: security csrf disable

--- src/main/java/com/study/realworld/global/config/WebSecurityConfig.java
+++ src/main/java/com/study/realworld/global/config/WebSecurityConfig.java
@@ -18,6 +18,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
         http.authorizeRequests()
                 .antMatchers("/api/users", "/api/users/login").permitAll()
                 .anyRequest().authenticated();
