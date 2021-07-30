c30e2102ca1a6de7f537d2fdd5bbaf7e379fc160
povia <47127607+povia@users.noreply.github.com>
2021-07-31 03:54:52 +0900

    setup: 패키지명 변경, jpa 추가

--- src/main/java/com/tistory/povia/realworld/RealworldApplication.java
+++ src/main/java/com/tistory/povia/realworld/RealworldApplication.java
@@ -1,4 +1,4 @@
-package com.study.realworld;
+package com.tistory.povia.realworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/test/java/com/tistory/povia/realworld/RealworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.povia.realworld;
+
+import com.tistory.povia.realworld.RealworldApplication;
+import org.junit.jupiter.api.Test;
+
+class RealworldApplicationTests {
+
+  @Test
+  void main() {
+    RealworldApplication.main(new String[] {});
+  }
+
+}
