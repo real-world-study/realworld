dd161d60e4489ed695f7e22c663d8418b1eccdf6
김우재 <kwj1270@naver.com>
2021-07-31 13:27:28 +0900

    [#5] test: User 인스턴스 getter() 테스트 작성

--- src/test/java/com/study/realworld/domain/user/dto/UserJoinRequestTest.java
+++ src/test/java/com/study/realworld/domain/user/dto/UserJoinRequestTest.java
@@ -1,5 +1,6 @@
 package com.study.realworld.domain.user.dto;
 
+import com.study.realworld.domain.user.domain.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,4 +19,24 @@ class UserJoinRequestTest {
                 () -> assertThat(userJoinRequest).isExactlyInstanceOf(UserJoinRequest.class)
         );
     }
+
+    @DisplayName("UserJoinRequest 인스턴스 toEntity() 테스트")
+    @Test
+    void toEntity_test() {
+        final String username = "username";
+        final String email = "email";
+        final String password = "password";
+        final UserJoinRequest userJoinRequest = new UserJoinRequest(username, email, password);
+        final User user =  userJoinRequest.toEntity();
+
+        assertAll(
+                () -> assertThat(user).isNotNull(),
+                () -> assertThat(user).isExactlyInstanceOf(User.class),
+                () -> assertThat(user.username()).isEqualTo(username),
+                () -> assertThat(user.email()).isEqualTo(email),
+                () -> assertThat(user.checkPassword(password))
+        );
+    }
+
+
 }
