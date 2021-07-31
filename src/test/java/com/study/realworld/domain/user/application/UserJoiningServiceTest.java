684c20447e5b284923a90ffca35fc3747c56036f
김우재 <kwj1270@naver.com>
2021-07-31 14:55:45 +0900

    [#5] test: UserJoiningService 인스턴스 join() 테스트 작성

--- src/test/java/com/study/realworld/domain/user/application/UserJoiningServiceTest.java
+++ src/test/java/com/study/realworld/domain/user/application/UserJoiningServiceTest.java
@@ -1,6 +1,8 @@
 package com.study.realworld.domain.user.application;
 
 import com.study.realworld.domain.user.domain.UserRepository;
+import com.study.realworld.domain.user.dto.UserJoinRequest;
+import com.study.realworld.domain.user.dto.UserJoinResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,8 +10,16 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static com.study.realworld.domain.user.domain.UserTest.USER;
+import static com.study.realworld.domain.user.dto.UserJoinRequestTest.USER_JOIN_REQUEST;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 class UserJoiningServiceTest {
@@ -28,6 +38,21 @@ class UserJoiningServiceTest {
         assertAll(
                 () -> assertThat(userJoiningService).isNotNull(),
                 () -> assertThat(userJoiningService).isExactlyInstanceOf(UserJoiningService.class)
+        );
+    }
+
+    @DisplayName("UserJoiningService 인스턴스 join() 테스트")
+    @Test
+    void join_test() {
+        given(userRepository.save(any())).willReturn(Optional.ofNullable(USER));
+
+        final UserJoinRequest userJoinRequest = USER_JOIN_REQUEST;
+        final UserJoinResponse userJoinResponse = userJoiningService.join(userJoinRequest);
+
+        then(userRepository).should(times(1)).save(any());
+        assertAll(
+                () -> assertThat(userJoinResponse).isNotNull(),
+                () -> assertThat(userJoinResponse).isExactlyInstanceOf(UserJoiningService.class)
         );
     }
 
