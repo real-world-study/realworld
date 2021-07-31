aaf352644c12eb90ceb3bff0cc9ed4d76b0cfbe1
김우재 <kwj1270@naver.com>
2021-07-31 14:28:55 +0900

    [#5] feat: UserJoinResponse 인스턴스 fromUser() 테스트 작성

--- src/main/java/com/study/realworld/domain/user/dto/UserJoinResponse.java
+++ src/main/java/com/study/realworld/domain/user/dto/UserJoinResponse.java
@@ -5,25 +5,11 @@ import com.fasterxml.jackson.annotation.JsonRootName;
 import com.study.realworld.domain.user.domain.User;
 
 @JsonRootName("user")
-public class UserJoinResponse {
-
-    @JsonProperty("email")
-    private String email;
-
-    @JsonProperty("username")
-    private String username;
-
-    @JsonProperty("bio")
-    private String bio;
-
-    @JsonProperty("image")
-    private String image;
+public final class UserJoinResponse {
 
     public static final UserJoinResponse fromUser(final User user) {
         return new UserJoinResponse(user);
     }
-
-    private UserJoinResponse() { }
 
     private UserJoinResponse(final User user) {
         this.email = user.email();
@@ -31,5 +17,17 @@ public class UserJoinResponse {
         this.bio = user.bio();
         this.image = user.image();
     }
+
+    @JsonProperty("email")
+    private final String email;
+
+    @JsonProperty("username")
+    private final String username;
+
+    @JsonProperty("bio")
+    private final String bio;
+
+    @JsonProperty("image")
+    private final String image;
 
 }
