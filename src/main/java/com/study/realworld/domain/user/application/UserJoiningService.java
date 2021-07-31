a4b8e13ece051a5675397a5aa9b300a6f0d7c085
김우재 <kwj1270@naver.com>
2021-07-31 13:42:12 +0900

    [#5] feat: UserJoiningService 인스턴스 생성자 기능 구현

--- src/main/java/com/study/realworld/domain/user/application/UserJoiningService.java
+++ src/main/java/com/study/realworld/domain/user/application/UserJoiningService.java
@@ -1,7 +1,14 @@
 package com.study.realworld.domain.user.application;
 
+import com.study.realworld.domain.user.domain.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserJoiningService {
+
+    private final UserRepository userRepository;
+
+    public UserJoiningService(final UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
