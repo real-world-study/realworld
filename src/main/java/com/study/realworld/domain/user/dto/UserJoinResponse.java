e26926a31c53f57bc3071d8a369d9f70155ff0d9
김우재 <kwj1270@naver.com>
2021-07-31 14:25:44 +0900

    [#5] test: UserJoinResponse 인스턴스 fromUser() 테스트 작성

---
+++ src/main/java/com/study/realworld/domain/user/dto/UserJoinResponse.java
@@ -0,0 +1,35 @@
+package com.study.realworld.domain.user.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import com.study.realworld.domain.user.domain.User;
+
+@JsonRootName("user")
+public class UserJoinResponse {
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("username")
+    private String username;
+
+    @JsonProperty("bio")
+    private String bio;
+
+    @JsonProperty("image")
+    private String image;
+
+    public static final UserJoinResponse fromUser(final User user) {
+        return new UserJoinResponse(user);
+    }
+
+    private UserJoinResponse() { }
+
+    private UserJoinResponse(final User user) {
+        this.email = user.email();
+        this.username = user.username();
+        this.bio = user.bio();
+        this.image = user.image();
+    }
+
+}
