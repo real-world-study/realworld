3950da5c3e0ab6cac569913cfb6055860f776609
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-08-04 06:14:08 +0900

    refactor: 미사용 import 제거

--- src/test/java/com/study/realworld/user/domain/UserTest.java
+++ src/test/java/com/study/realworld/user/domain/UserTest.java
@@ -1,16 +1,14 @@
 package com.study.realworld.user.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import static java.lang.String.valueOf;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class UserTest {
