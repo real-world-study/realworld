95188f8e422ee9c9582fa667f44ee00380e4ba96
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-08-03 16:09:24 +0900

    feat: JwtTokenProvider 전체 기능 구현 및 테스트 작성

--- src/main/java/com/study/realworld/jwt/JwtTokenProvider.java
+++ src/main/java/com/study/realworld/jwt/JwtTokenProvider.java
@@ -1,14 +1,21 @@
 package com.study.realworld.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
 import java.util.Date;
-import org.springframework.security.core.Authentication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class JwtTokenProvider {
+
+    private final Logger log = LoggerFactory.getLogger(JwtTokenProvider.class);
 
     private final Key key;
     private final String headerType;
@@ -23,8 +30,7 @@ public class JwtTokenProvider {
         this.accessTime = accessTime;
     }
 
-    // TODO : payload sub값 차후 pk로 변경 필요 + 사용자 정보 claim 추가 필요
-    public String generateToken(Authentication authentication) {
+    public String generateToken(JwtAuthentication authentication) {
         long now = (new Date()).getTime();
 
         Date issuedAtDate = new Date();
@@ -32,13 +38,46 @@ public class JwtTokenProvider {
         String accessToken = Jwts.builder()
             .signWith(key, SignatureAlgorithm.HS512)            // header "alg" : HS512
             .setHeaderParam("typ", headerType)            // header "typ" : JWT
-            .setSubject(authentication.getName())               // payload "sub" : username
+            .setSubject(authentication.getId().toString())      // payload "sub" : userId
             .setIssuer(issuer)                                  // payload "iss" : ori
             .setExpiration(accessTokenExpiresIn)                // payload "exp"
             .setIssuedAt(issuedAtDate)                          // payload "iat"
             .compact();
 
         return accessToken;
+    }
+
+    public JwtAuthenticationToken getAuthentication(String accessToken) {
+        Claims claims = parseClaims(accessToken);
+
+        Long userId = Long.parseLong(claims.getSubject());
+
+        return new JwtAuthenticationToken(new JwtAuthentication(userId), null);
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+            log.info("잘못된 JWT 서명입니다.");
+        } catch (ExpiredJwtException e) {
+            log.info("만료된 JWT 토큰입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.info("지원되지 않는 JWT 토큰입니다.");
+        } catch (IllegalArgumentException e) {
+            log.info("JWT 토큰이 잘못되었습니다.");
+        }
+        return false;
+    }
+
+    private Claims parseClaims(String accessToken) {
+        try {
+            return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken)
+                .getBody();
+        } catch (ExpiredJwtException e) {
+            throw new RuntimeException("권한 정보가 없는 토큰입니다.");
+        }
     }
 
 }
--- src/test/java/com/study/realworld/jwt/JwtTokenProviderTest.java
+++ src/test/java/com/study/realworld/jwt/JwtTokenProviderTest.java
@@ -1,42 +1,180 @@
 package com.study.realworld.jwt;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.security.Key;
+import java.util.Date;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.security.core.Authentication;
 
 @ExtendWith(MockitoExtension.class)
 class JwtTokenProviderTest {
 
     @Mock
-    private Authentication authentication;
+    private JwtAuthentication authentication;
 
     private JwtTokenProvider jwtTokenProvider;
 
+    private String headerType = "headerType";
+    private String issuser = "issuer";
+    private String secret = "dGVzdF90ZXN0X3Rlc3RfdGVzdF90ZXN0X3Rlc3RfdGVzdF90ZXN0X3Rlc3RfdGVzdF90ZXN0X3Rlc3RfdGVzdF8K";
+    private long accessTime = 1000L;
+
     @BeforeEach
     void beforeEach() {
-        jwtTokenProvider = new JwtTokenProvider("headerType", "issuer",
-            "dGVzdF90ZXN0X3Rlc3RfdGVzdF90ZXN0X3Rlc3RfdGVzdF90ZXN0X3Rlc3RfdGVzdF90ZXN0X3Rlc3RfdGVzdF8K", 10L);
+        jwtTokenProvider = new JwtTokenProvider(headerType, issuser, secret, accessTime);
     }
 
     @Test
-    @DisplayName("generateToken으로 토큰이 생성되어야 한다.")
+    @DisplayName("generateToken()으로 토큰이 생성된다.")
     void generateTokenTest() {
 
-        // setup & given
-        when(authentication.getName()).thenReturn("username");
+        // setup && given
+        when(authentication.getId()).thenReturn(1L);
 
         // when
         String result = jwtTokenProvider.generateToken(authentication);
 
         // then
         assertThat(result).isNotNull();
+    }
+
+    @Test
+    @DisplayName("입력받은 토큰으로 유저의 Authentication을 반환할 수 있다.")
+    void getAuthenticationTest() {
+
+        // given
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        Key key = Keys.hmacShaKeyFor(keyBytes);
+        long now = (new Date()).getTime();
+        Long id = 2L;
+        String accessToken = Jwts.builder()
+            .setSubject(id.toString())
+            .setExpiration(new Date(now + 3000))
+            .signWith(key, SignatureAlgorithm.HS512)
+            .compact();
+
+        // when
+        JwtAuthenticationToken authentication = jwtTokenProvider.getAuthentication(accessToken);
+        JwtAuthentication result = (JwtAuthentication) authentication.getPrincipal();
+
+        // then
+        assertThat(result.getId()).isEqualTo(id);
+    }
+
+    @Test
+    @DisplayName("입력받은 토큰이 만료되면 Authentication을 반환할 수 없고 Exception이 발생해야 한다.")
+    void getAuthenticationByExpiredTokenTest() {
+
+        // given
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        Key key = Keys.hmacShaKeyFor(keyBytes);
+        long now = (new Date()).getTime();
+        Long id = 2L;
+        String accessToken = Jwts.builder()
+            .setSubject(id.toString())
+            .setExpiration(new Date(now - 3000))
+            .signWith(key, SignatureAlgorithm.HS512)
+            .compact();
+
+        // when & then
+        assertThatExceptionOfType(Exception.class)
+            .isThrownBy(() -> jwtTokenProvider.getAuthentication(accessToken));
+    }
+
+    @Test
+    @DisplayName("정상적인 토큰은 true를 반환한다.")
+    void testValidateTokenByTrueToken() {
+
+        // given
+        byte[] keyBytes = Decoders.BASE64.decode(secret);
+        Key key = Keys.hmacShaKeyFor(keyBytes);
+        long now = (new Date()).getTime();
+        String accessToken = Jwts.builder()
+            .setExpiration(new Date(now + 3000))
+            .signWith(key, SignatureAlgorithm.HS512)
+            .compact();
+
+        // when
+        boolean validateion = jwtTokenProvider.validateToken(accessToken);
+
+        // then
+        assertTrue(validateion);
+    }
+
+    @Test
+    @DisplayName("잘못 입력된 토큰은 false를 반환한다.")
+    void testValidateTokenByMalFormedToken() {
+
+        // given
+        String accessToken = "te.st.";
+
+        // when
+        boolean validation = jwtTokenProvider.validateToken(accessToken);
+
+        // then
+        assertFalse(validation);
+    }
+
+    @Test
+    @DisplayName("만료된 토큰은 false를 반환한다.")
+    void testValidateTokenByExpiredToken() {
+
+        // given
+        long now = (new Date()).getTime();
+        String accessToken = Jwts.builder()
+            .setExpiration(new Date(now - 100))
+            .compact();
+
+        // when
+        boolean validation = jwtTokenProvider.validateToken(accessToken);
+
+        // then
+        assertFalse(validation);
+    }
+
+    @Test
+    @DisplayName("지원하지 않는 토큰은 false를 반환한다.")
+    void testValidateTokenByUnsupportedToken() {
+
+        // given
+        long now = (new Date()).getTime();
+        String accessToken = Jwts.builder()
+            .claim("auth2" , "testauth")
+            .setExpiration(new Date(now + 3000))
+            .compact();
+
+        // when
+        boolean validation = jwtTokenProvider.validateToken(accessToken);
+
+        // then
+        assertFalse(validation);
+    }
+
+    @Test
+    @DisplayName("토큰의 형태가 잘못 들어온 토큰은 false를 반환한다.")
+    void testValidateTokenByIllegalArgumentToken() {
+
+        // given
+        String accessToken = "";
+
+        // when
+        boolean validation = jwtTokenProvider.validateToken(accessToken);
+
+        // then
+        assertFalse(validation);
     }
 
 }
