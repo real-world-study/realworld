ea16e7bedc01fbb8638758dd0160ebb717a879f3
Hoony <44036052+hoony-lab@users.noreply.github.com>
2021-08-03 11:47:45 +0900

    [#12] feat: User Entity 추가

---
+++ src/main/java/com/study/realworld/user/domain/User.java
@@ -0,0 +1,79 @@
+package com.study.realworld.user.domain;
+
+import com.study.realworld.global.common.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, updatable = false)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String email;
+
+    @Column(nullable = false, length = 50)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String bio;
+
+    @Column
+    private String image;
+
+    @Builder
+    public User(Long id, String email, String username, String password, String bio, String image) {
+        this.id = id;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+        this.bio = bio;
+        this.image = image;
+    }
+
+    public static User of(User user) {
+        return User.builder()
+                .email(user.getEmail())
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .bio(user.getBio())
+                .image(user.getImage())
+                .build();
+    }
+
+    public void encodePassword(PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(password);
+    }
+
+    public boolean matchesPassword(String rawPassword, PasswordEncoder passwordEncoder) {
+        return passwordEncoder.matches(rawPassword, this.password);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(email, user.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(email);
+    }
+
+}
