35d86b6de1e391fe41fb674d4befaae724c1aea2
Hoony <44036052+hoony-lab@users.noreply.github.com>
2021-08-07 20:39:50 +0900

    [#12] feat: UserNotFoundException 삭제

--- src/main/java/com/study/realworld/user/exception/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.study.realworld.user.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
