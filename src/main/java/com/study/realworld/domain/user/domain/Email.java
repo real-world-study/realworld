982db6871020994989debb5d744e9808528b0609
김우재 <kwj1270@naver.com>
2021-08-08 19:26:09 +0900

    [#20] feat: Email 인스턴스 생성자 기능 구현

--- src/main/java/com/study/realworld/domain/user/domain/Email.java
+++ src/main/java/com/study/realworld/domain/user/domain/Email.java
@@ -1,4 +1,18 @@
 package com.study.realworld.domain.user.domain;
 
+import javax.validation.constraints.NotBlank;
+
 public class Email {
+
+    @javax.validation.constraints.Email
+    @NotBlank
+    private String email;
+
+    protected Email() {
+    }
+
+    public Email(final String email) {
+        this.email = email;
+    }
+
 }
