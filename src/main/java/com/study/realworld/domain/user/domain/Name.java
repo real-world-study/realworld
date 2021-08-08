bed53dfaa2bf0c27f4c752de09f5480ace430ca8
김우재 <kwj1270@naver.com>
2021-08-08 22:25:17 +0900

    [#20] feat: Name 인스턴스 생성자 기능 구현

--- src/main/java/com/study/realworld/domain/user/domain/Name.java
+++ src/main/java/com/study/realworld/domain/user/domain/Name.java
@@ -1,4 +1,16 @@
 package com.study.realworld.domain.user.domain;
 
+import javax.persistence.Embeddable;
+
+@Embeddable
 public class Name {
+
+    private String name;
+
+    protected Name() { }
+
+    public Name(final String name) {
+        this.name = name;
+    }
+
 }
