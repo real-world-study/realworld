812e27f2598b92f25a3895ce77b02db8a5338e04
Hoony <44036052+hoony-lab@users.noreply.github.com>
2021-08-09 01:06:54 +0900

    [#12] proposal
    Constructor가 필요한 클래스일까요?
    from jinyoungchoi95
    
    Co-authored-by: 최진영 <jinyoungchoi95@gmail.com>

--- src/main/java/com/study/realworld/config/SecurityConfig.java
+++ src/main/java/com/study/realworld/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
