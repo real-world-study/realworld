a1b1e0281cdfd49597e88747acc0cc4595e37a7b
김우재 <kwj1270@naver.com>
2021-08-09 00:48:41 +0900

    [#20] test: Password 인스턴스 정적 팩터리 메서드 생성 테스트 작성

--- src/main/java/com/study/realworld/domain/user/domain/Password.java
+++ src/main/java/com/study/realworld/domain/user/domain/Password.java
@@ -1,4 +1,19 @@
 package com.study.realworld.domain.user.domain;
 
+import javax.persistence.Embeddable;
+import javax.validation.constraints.NotBlank;
+
+@Embeddable
 public class Password {
+
+    @NotBlank
+    private String password;
+
+    protected Password() {
+    }
+
+    public Password(String password) {
+        this.password = password;
+    }
+
 }
--- src/test/java/com/study/realworld/domain/user/domain/PasswordTest.java
+++ src/test/java/com/study/realworld/domain/user/domain/PasswordTest.java
@@ -1,12 +1,32 @@
 package com.study.realworld.domain.user.domain;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PasswordTest {
+
+    private static PasswordEncoder passwordEncoder;
+
+    @BeforeAll
+    void setUp() {
+        passwordEncoder = new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence plainTextPassword) {
+                return BCrypt.hashpw(plainTextPassword.toString(),BCrypt.gensalt(8));
+            }
+
+            @Override
+            public boolean matches(CharSequence plainTextPassword, String passwordInDatabase) {
+                return BCrypt.checkpw(plainTextPassword.toString(),passwordInDatabase);
+            }
+        };
+    }
 
     @DisplayName("Password 인스턴스 기본 생성자 테스트")
     @Test
@@ -18,4 +38,30 @@ class PasswordTest {
                 () -> assertThat(password).isExactlyInstanceOf(Password.class)
         );
     }
+
+    @DisplayName("Password 인스턴스 정적 팩터리 메서드 생성 테스트")
+    @Test
+    void static_factory_method_test() {
+        final String passwordString = "password";
+        final Password password = Password.createWithEncoder(passwordString, passwordEncoder);
+
+        assertAll(
+                () -> assertThat(password).isNotNull(),
+                () -> assertThat(password).isExactlyInstanceOf(Password.class)
+        );
+    }
+
+//    @DisplayName("Password 인스턴스 값이 비교 대상과 같은지 검증 테스트")
+//    @Test
+//    void checkPassword_test() {
+//        final String passwordString = "password";
+//        final String invalidPasswordString = "invalidPassword";
+//        final Password password = Password.createWithEncoder(passwordString, passwordEncoder);
+//
+//        assertAll(
+//                () -> assertThat(password.checkPassword(passwordString, passwordEncoder)).isTrue(),
+//                () -> assertThat(password.checkPassword(invalidPasswordString, passwordEncoder)).isFalse()
+//        );
+//    }
+
 }
