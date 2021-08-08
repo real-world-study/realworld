5ce4327446cd55bc01a51fd4798e270dfe87bfa2
김우재 <kwj1270@naver.com>
2021-08-08 20:18:25 +0900

    [#20] test: Email 인스턴스 값이 이메일 형식이 맞는지 테스트 작성

--- src/test/java/com/study/realworld/domain/user/domain/EmailTest.java
+++ src/test/java/com/study/realworld/domain/user/domain/EmailTest.java
@@ -1,12 +1,27 @@
 package com.study.realworld.domain.user.domain;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmailTest {
+
+    private static Validator validator;
+
+    @BeforeAll
+    static void setUp() {
+        validator = Validation.buildDefaultValidatorFactory().getValidator();
+    }
 
     @DisplayName("Email 인스턴스 기본 생성자 테스트")
     @Test
@@ -40,4 +55,17 @@ class EmailTest {
         assertThat(email.email()).isEqualTo(emailString);
     }
 
+
+    @DisplayName("Email 인스턴스 값이 이메일 형식이 맞는지 테스트")
+    @ValueSource(strings = {"test_test.com", "test@@test", "@test.com"})
+    @ParameterizedTest
+    void email_format_test(final String emailString) {
+        final Email email = new Email(emailString);
+        final Set<ConstraintViolation<Email>> violations = validator.validate(email);
+
+        assertAll(
+                () -> assertThat(violations.size()).isGreaterThanOrEqualTo(1),
+                () -> assertThat(violations.iterator().next().getMessage()).isEqualTo("Invalid email address")
+        );
+    }
 }
