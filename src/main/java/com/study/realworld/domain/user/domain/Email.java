6a54477176a82281bf6a2221abf911d92e7ecee9
김우재 <kwj1270@naver.com>
2021-08-08 20:46:53 +0900

    [#20] feat: Email 인스턴스 값 공백 검증 기능 구현

--- src/main/java/com/study/realworld/domain/user/domain/Email.java
+++ src/main/java/com/study/realworld/domain/user/domain/Email.java
@@ -1,11 +1,15 @@
 package com.study.realworld.domain.user.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
 import javax.validation.constraints.NotBlank;
 
+@Embeddable
 public class Email {
 
     @javax.validation.constraints.Email(message = "Invalid email address")
     @NotBlank(message = "Email must have not blank")
+    @Column(name = "email")
     private String email;
 
     protected Email() {
