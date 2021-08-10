b014b792c022f7ab5f5f786c729cae6b29ee9a7a
김우재 <kwj1270@naver.com>
2021-08-11 02:08:28 +0900

    [#20] test: Bio 인스턴스 기본 생성자 테스트 작성

---
+++ src/test/java/com/study/realworld/domain/user/domain/BioTest.java
@@ -0,0 +1,22 @@
+package com.study.realworld.domain.user.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BioTest {
+
+    @DisplayName("Bio 인스턴스 기본 생성자 테스트")
+    @Test
+    void default_constructor_test() {
+        final Bio bio = new Bio();
+
+        assertAll(
+                () -> assertThat(bio).isNotNull(),
+                () -> assertThat(bio).isExactlyInstanceOf(Bio.class)
+        );
+    }
+
+}
