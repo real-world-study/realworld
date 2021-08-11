cec6f69ba549d3cb750b67d990f5cfd5c872ad8e
Hoony <44036052+hoony-lab@users.noreply.github.com>
2021-08-11 11:54:07 +0900

    [#12] feat: UserRepositoryTest 추가
    `@DataJpaTest` 에 `JpaConfig` 추가해서 `BasetimeEntity` 테스트

---
+++ src/test/java/com/study/realworld/user/domain/UserRepositoryTest.java
@@ -0,0 +1,54 @@
+package com.study.realworld.user.domain;
+
+import com.study.realworld.config.JpaConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest(includeFilters = @ComponentScan.Filter(
+        type = FilterType.ASSIGNABLE_TYPE,
+        classes = JpaConfig.class
+))
+class UserRepositoryTest {
+
+    private static final Long ID = 1L;
+    private static final String EMAIL = "email";
+    private static final String USERNAME = "username";
+    private static final String PASSWORD = "password";
+    private static final String BIO = "bio";
+    private static final String IMAGE = "image";
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    void save() {
+
+        // given
+        final User user = User.builder()
+                .id(ID)
+                .email(EMAIL)
+                .username(USERNAME)
+                .password(PASSWORD)
+                .bio(BIO)
+                .image(IMAGE)
+                .build();
+
+        // when
+        User result = userRepository.save(user);
+
+        // then
+        assertAll(
+                () -> assertEquals(user.getEmail(), result.getEmail()),
+                () -> assertEquals(user.getUsername(), result.getUsername()),
+                () -> assertTrue(result.getCreatedAt() != null),
+                () -> assertTrue(result.getUpdatedAt() != null),
+                () -> assertTrue(result.getDeletedAt() == null)
+        );
+    }
+
+}
