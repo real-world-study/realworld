e0ad675345ae15bb7db323be4c9009dd24201709
김우재 <kwj1270@naver.com>
2021-08-12 00:04:23 +0900

    [#20] feat: Image 인스턴스 equals and hashcode 동등성 검증 기능 구현

--- src/main/java/com/study/realworld/domain/user/domain/Image.java
+++ src/main/java/com/study/realworld/domain/user/domain/Image.java
@@ -1,5 +1,7 @@
 package com.study.realworld.domain.user.domain;
 
+import java.util.Objects;
+
 public class Image {
 
     private String path;
@@ -13,6 +15,19 @@ public class Image {
 
     public String path() {
         return path;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final Image image = (Image) o;
+        return Objects.equals(path(), image.path());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(path());
     }
 
 }
