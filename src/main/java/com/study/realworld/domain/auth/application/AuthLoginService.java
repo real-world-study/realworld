30ca9506d6068b852d2d0672d8123b8df04603e7
김우재 <kwj1270@naver.com>
2021-08-14 14:42:07 +0900

    [#20] feat: AuthLoginService 인스턴스 생성 기능 구현

--- src/main/java/com/study/realworld/domain/auth/application/AuthLoginService.java
+++ src/main/java/com/study/realworld/domain/auth/application/AuthLoginService.java
@@ -1,4 +1,13 @@
 package com.study.realworld.domain.auth.application;
 
+import com.study.realworld.domain.user.domain.UserRepository;
+
 public class AuthLoginService {
+
+    private final UserRepository userRepository;
+
+    public AuthLoginService(final UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
 }
--- src/test/java/com/study/realworld/domain/auth/application/AuthLoginServiceTest.java
+++ src/test/java/com/study/realworld/domain/auth/application/AuthLoginServiceTest.java
@@ -30,4 +30,6 @@ class AuthLoginServiceTest {
         );
     }
 
+
+
 }
