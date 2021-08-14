0f3c983f7c19e9dbeedeb87104471922840e1373
김우재 <kwj1270@naver.com>
2021-08-14 14:00:51 +0900

    [#20] test: LoginRequest 인스턴스 생성자 테스트 작성

--- src/test/java/com/study/realworld/domain/auth/dto/LoginRequestTest.java
+++ src/test/java/com/study/realworld/domain/auth/dto/LoginRequestTest.java
@@ -1,17 +1,33 @@
 package com.study.realworld.domain.auth.dto;
 
+import com.study.realworld.domain.user.domain.EmailTest;
+import com.study.realworld.domain.user.domain.Password;
+import com.study.realworld.domain.user.domain.PasswordTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.study.realworld.domain.user.domain.EmailTest.EMAIL;
+import static com.study.realworld.domain.user.domain.PasswordTest.PASSWORD;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class LoginRequestTest {
 
+    @DisplayName("LoginRequest 인스턴스 기본 생성자 테스트")
+    @Test
+    void default_constructor_test() {
+        final LoginRequest loginRequest = new LoginRequest();
+
+        assertAll(
+                () -> assertThat(loginRequest).isNotNull(),
+                () -> assertThat(loginRequest).isExactlyInstanceOf(LoginRequest.class)
+        );
+    }
+
     @DisplayName("LoginRequest 인스턴스 생성자 테스트")
     @Test
     void constructor_test() {
-        final LoginRequest loginRequest = new LoginRequest();
+        final LoginRequest loginRequest = new LoginRequest(EMAIL, PASSWORD);
 
         assertAll(
                 () -> assertThat(loginRequest).isNotNull(),
