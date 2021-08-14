6fcd51b662b32f0ee64d13bc5154a97a3b485a8e
김우재 <kwj1270@naver.com>
2021-08-14 17:15:06 +0900

    [#20] feat: 통합 테스트 AuthController 인스턴스 login 기능 구현

---
+++ src/test/java/com/study/realworld/domain/auth/application/presentation/AuthControllerTest.java
@@ -0,0 +1,50 @@
+package com.study.realworld.domain.auth.application.presentation;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.study.realworld.domain.auth.application.AuthLoginService;
+import com.study.realworld.domain.auth.dto.LoginRequest;
+import com.study.realworld.domain.user.domain.*;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static com.study.realworld.domain.auth.dto.LoginRequestTest.loginRequest;
+import static com.study.realworld.domain.user.domain.BioTest.BIO;
+import static com.study.realworld.domain.user.domain.EmailTest.EMAIL;
+import static com.study.realworld.domain.user.domain.ImageTest.IMAGE;
+import static com.study.realworld.domain.user.domain.NameTest.USERNAME;
+import static com.study.realworld.domain.user.domain.PasswordTest.PASSWORD;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc(addFilters = false)
+@WebMvcTest(AuthController.class)
+class AuthControllerTest {
+
+    @Autowired private MockMvc mockMvc;
+    @Autowired private ObjectMapper objectMapper;
+    @MockBean private AuthLoginService authLoginService;
+
+    @DisplayName("AuthController 인스턴스 login 테스트")
+    @Test
+    void login_test() throws Exception {
+        final User user = UserTest.userBuilder(new Email(EMAIL), new Name(USERNAME), new Password(PASSWORD), new Bio(BIO), new Image(IMAGE));
+        final LoginRequest loginRequest = loginRequest(new Email(EMAIL), new Password(PASSWORD));
+        final String loginRequestString = objectMapper.writeValueAsString(loginRequest);
+        given(authLoginService.login(any(), any())).willReturn(user);
+
+        mockMvc.perform(post("/api/users/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(loginRequestString))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+}
