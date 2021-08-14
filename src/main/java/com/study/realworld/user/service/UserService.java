b8b81a592c134afb65f5648737ee3125b9a52b7d
Hoony <44036052+hoony-lab@users.noreply.github.com>
2021-08-14 12:49:33 +0900

    [#12] fix: UserService에 Transactional 수정
    `@Transactional은 보통 Class단에 어노테이션으로 먹일 때에는 readOnly = true, CUD 메소드에 먹일 때 readOnly = false로 먹이니 참고만 해주셔용`
    
    Co-Authored-By: povia <47127607+povia@users.noreply.github.com>

--- src/main/java/com/study/realworld/user/service/UserService.java
+++ src/main/java/com/study/realworld/user/service/UserService.java
@@ -14,30 +14,28 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 @Service
 public class UserService {
 
     private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
-    @Transactional(readOnly = true)
     public List<User> findAll() {
         return userRepository.findAll();
     }
 
-    @Transactional(readOnly = true)
     public User findById(Long id) {
         return userRepository.findById(id)
                 .orElseThrow(() -> new NoSuchElementException(id + " not found"));
     }
 
-    @Transactional(readOnly = true)
     public User findByEmail(String email) {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new NoSuchElementException(email + " not found"));
     }
 
+    @Transactional
     public User save(UserJoinRequest request) {
         User user = UserJoinRequest.toUser(request);
         user.encodePassword(passwordEncoder);
@@ -45,6 +43,7 @@ public class UserService {
         return userRepository.save(user);
     }
 
+    @Transactional
     public User deleteById(Long id) {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new NoSuchElementException(id + " not found"));
@@ -53,7 +52,6 @@ public class UserService {
         return user;
     }
 
-    @Transactional(readOnly = true)
     public User login(UserLoginRequest request) {
         User user = this.findByEmail(request.getEmail());
         validateMatchesPassword(user, request.getPassword());
