ae836d9a0746fe67a890955bbf949ec8135babea
povia <47127607+povia@users.noreply.github.com>
2021-08-17 08:32:11 +0900

    povia-issue14 fix: 주석 삭제

--- src/main/java/com/tistory/povia/realworld/configuration/SecurityConfig.java
+++ src/main/java/com/tistory/povia/realworld/configuration/SecurityConfig.java
@@ -26,8 +26,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final JwtRequestFilter jwtRequestFilter;
 
-    //  private final MyUserDetailsService userDetailService;
-    //
     public SecurityConfig(
             JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint,
             UserDetailsService userDetailsService,
@@ -36,16 +34,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.userDetailsService = userDetailsService;
         this.jwtRequestFilter = jwtRequestFilter;
     }
-    //
-    //  @Override
-    //  protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-    //    auth.userDetailsService(userService);
-    //  }
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        // configure AuthenticationManager so that it knows from where to load
-        // user for matching credentials
-        // Use BCryptPasswordEncoder
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 
@@ -62,19 +53,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // We don't need CSRF for this example
         http.csrf()
                 .disable()
-                // dont authenticate this particular request
                 .authorizeRequests()
                 .antMatchers("/authenticate")
                 .permitAll()
                 .anyRequest()
-                // all other requests need to be authenticated
                 .authenticated()
                 .and()
-                // make sure we use stateless session; session won't be used to
-                // store user's state.
                 .exceptionHandling()
                 .authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .and()
@@ -83,35 +69,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
-
-    //  @Override
-    //  protected void configure(HttpSecurity http) throws Exception {
-    //    http
-    //      .csrf()
-    //      .disable()
-    //      .headers()
-    //      .disable()
-    //      .exceptionHandling()
-    //      .accessDeniedHandler(accessDeniedHandler)
-    //      .authenticationEntryPoint(unauthorizedHandler)
-    //      .and()
-    //      .sessionManagement()
-    //      .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-    //      .and()
-    //      .authorizeRequests()
-    //      .antMatchers("/api/_hcheck").permitAll()
-    //      .antMatchers("/api/auth").permitAll()
-    //      .antMatchers("/api/user/join").permitAll()
-    //      .antMatchers("/api/user/exists").permitAll()
-    //      .antMatchers("/api/**").hasRole(Role.USER.name())
-    //      .accessDecisionManager(accessDecisionManager())
-    //      .anyRequest().permitAll()
-    //      .and()
-    //      .formLogin()
-    //      .disable();
-    //    http
-    //      .addFilterBefore(jwtAuthenticationTokenFilter(),
-    // UsernamePasswordAuthenticationFilter.class);
-    //  }
-
 }
