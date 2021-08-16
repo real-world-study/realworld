acafbe70ef974ebdea68a400d9275a512ff741f5
DolphaGo <adamdoha@naver.com>
2021-08-16 09:16:49 +0900

    test: contextLoads

--- realworld-api/src/test/java/com/study/realworld/RealWorldApplicationTest.java
+++ realworld-api/src/test/java/com/study/realworld/RealWorldApplicationTest.java
@@ -1,13 +1,10 @@
 package com.study.realworld;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class RealWorldApplicationTest {
     @Test
     void contextLoads() {
+        RealWorldApplication.main(new String[] {});
     }
 }
