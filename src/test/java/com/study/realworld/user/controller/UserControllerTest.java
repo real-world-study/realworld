9bb2f86d14daf9cb83e7d9eeb0275090d560a5cc
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-08-20 10:08:50 +0900

    [#16] refactor

--- src/test/java/com/study/realworld/user/controller/UserControllerTest.java
+++ src/test/java/com/study/realworld/user/controller/UserControllerTest.java
@@ -35,7 +35,6 @@ import org.springframework.http.MediaType;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.payload.JsonFieldType;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
