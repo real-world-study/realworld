4a974530bfaf671e1698f2fa4f96d2c74c3d2a83
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-08-21 23:21:33 +0900

    feat: User 도메인 생성

---
+++ src/main/java/com/study/realworld/user/domain/User.java
@@ -0,0 +1,32 @@
+package com.study.realworld.user.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    private String password;
+
+    private String bio;
+
+    private String image;
+}
