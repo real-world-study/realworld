2ee1c3240c4752278a3f78c151ac5a8ce4b70447
Hoony <44036052+hoony-lab@users.noreply.github.com>
2021-08-26 21:36:38 +0900

    [#23] feat: addFilter JwtAuthenticationFilter

--- src/main/java/com/study/realworld/config/SecurityConfig.java
+++ src/main/java/com/study/realworld/config/SecurityConfig.java
@@ -1,13 +1,8 @@
 package com.study.realworld.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.study.realworld.config.auth.JwtAuthenticationFilter;
-import com.study.realworld.config.auth.JwtAuthenticationProvider;
-import com.study.realworld.config.auth.JwtUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,8 +15,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final ObjectMapper objectMapper;
-    private final JwtUserDetailsService jwtUserDetailsService;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -30,51 +24,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
                 .headers().frameOptions().disable()
                 .and()
-                    .authorizeRequests()
-                    .antMatchers("/api/hello", "/h2-console/**").permitAll()
-                    .antMatchers("/api/users/**", "/api/users/login/**").permitAll()
-                    .antMatchers("/api/articles/**").permitAll()
-                    .antMatchers("/api/tags/**").permitAll()
-                    .antMatchers("/error").permitAll()
-                    .anyRequest().authenticated()
+                .authorizeRequests()
+                .antMatchers("/api/hello", "/h2-console/**").permitAll()
+                .antMatchers("/api/users/**", "/api/users/login/**").permitAll()
+                .antMatchers("/api/articles/**").permitAll()
+                .antMatchers("/api/tags/**").permitAll()
+                .antMatchers("/error").permitAll()
+                .anyRequest().authenticated()
                 .and()
-                    .sessionManagement()
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-//                .authenticationProvider(jwtAuthenticationProvider())
-                .userDetailsService(jwtUserDetailsService)
-                .passwordEncoder(passwordEncoder());
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+        ;
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    protected AuthenticationManager authenticationManager() throws Exception {
-        return super.authenticationManager();
-    }
-
-    @Bean
-    public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
-        JwtAuthenticationFilter jwtAuthenticationFilter =
-                new JwtAuthenticationFilter(authenticationManager(), objectMapper);
-        jwtAuthenticationFilter.setFilterProcessesUrl("/api/users/login");
-//        jwtAuthenticationFilter.setAuthenticationSuccessHandler(loginSuccessHandler());
-        jwtAuthenticationFilter.afterPropertiesSet();
-        return jwtAuthenticationFilter;
-    }
-
-    @Bean
-    public JwtAuthenticationProvider jwtAuthenticationProvider() {
-        return new JwtAuthenticationProvider(passwordEncoder(), jwtUserDetailsService);
     }
 
 }
