36e0bb6cef356b43dc96d3fb50c07cdf13ea1d99
김우재 <kwj1270@naver.com>
2021-08-27 23:44:10 +0900

    [#20] refactor: join 트랜잭션 추가

--- src/main/java/com/study/realworld/domain/user/application/UserJoinService.java
+++ src/main/java/com/study/realworld/domain/user/application/UserJoinService.java
@@ -4,6 +4,7 @@ import com.study.realworld.domain.user.domain.User;
 import com.study.realworld.domain.user.domain.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserJoinService {
@@ -16,6 +17,7 @@ public class UserJoinService {
         this.passwordEncoder = passwordEncoder;
     }
 
+    @Transactional
     public User join(final User user) {
         final User encodedUser = user.encode(passwordEncoder);
         return userRepository.save(encodedUser);
