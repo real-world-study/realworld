827a1bec6c8733c3df99309352b2ddfc1cece55b
김우재 <kwj1270@naver.com>
2021-08-28 10:43:53 +0900

    [#40] feat: UserUpdateRequest 생성자 구현

--- src/main/java/com/study/realworld/domain/user/dto/UserUpdateRequest.java
+++ src/main/java/com/study/realworld/domain/user/dto/UserUpdateRequest.java
@@ -1,4 +1,28 @@
 package com.study.realworld.domain.user.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.study.realworld.domain.user.domain.Bio;
+import com.study.realworld.domain.user.domain.Email;
+import com.study.realworld.domain.user.domain.Image;
+
 public class UserUpdateRequest {
+
+    @JsonProperty("email")
+    private Email email;
+
+    @JsonProperty("bio")
+    private Bio bio;
+
+    @JsonProperty("image")
+    private Image image;
+
+    UserUpdateRequest() {
+    }
+
+    UserUpdateRequest(final Email email, final Bio bio, final Image image) {
+        this.email = email;
+        this.bio = bio;
+        this.image = image;
+    }
+
 }
