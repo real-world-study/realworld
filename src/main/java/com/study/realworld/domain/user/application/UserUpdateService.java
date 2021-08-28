811b199067741e16c217928934af9841d9e9913b
김우재 <kwj1270@naver.com>
2021-08-28 10:30:55 +0900

    [#40] feat: UserUpdateService 생성자 구현

--- src/main/java/com/study/realworld/domain/user/application/UserUpdateService.java
+++ src/main/java/com/study/realworld/domain/user/application/UserUpdateService.java
@@ -1,9 +1,17 @@
 package com.study.realworld.domain.user.application;
 
+import com.study.realworld.domain.user.domain.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional(readOnly = true)
 @Service
 public class UserUpdateService {
+
+    private final UserRepository userRepository;
+
+    public UserUpdateService(final UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
 }
