011be9c8af5f25b74ba363f7539ca8ab4d5184e5
김우재 <kwj1270@naver.com>
2021-08-28 10:42:06 +0900

    [#40] test: UserUpdateRequest 생성자 테스트 작성

--- src/test/java/com/study/realworld/domain/user/dto/UserUpdateRequestTest.java
+++ src/test/java/com/study/realworld/domain/user/dto/UserUpdateRequestTest.java
@@ -1,8 +1,14 @@
 package com.study.realworld.domain.user.dto;
 
+import com.study.realworld.domain.user.domain.Bio;
+import com.study.realworld.domain.user.domain.Email;
+import com.study.realworld.domain.user.domain.Image;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.study.realworld.domain.user.domain.BioTest.BIO;
+import static com.study.realworld.domain.user.domain.EmailTest.EMAIL;
+import static com.study.realworld.domain.user.domain.ImageTest.IMAGE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,6 +18,18 @@ class UserUpdateRequestTest {
     @Test
     void default_constructor_test() {
         final UserUpdateRequest userUpdateRequest = new UserUpdateRequest();
+
+        assertAll(
+                () -> assertThat(userUpdateRequest).isNotNull(),
+                () -> assertThat(userUpdateRequest).isExactlyInstanceOf(UserUpdateRequest.class)
+        );
+    }
+
+    @DisplayName("UserUpdateRequest 생성자 테스트")
+    @Test
+    void constructor_test() {
+        final UserUpdateRequest userUpdateRequest =
+                new UserUpdateRequest(new Email(EMAIL), new Bio(BIO), new Image(IMAGE));
 
         assertAll(
                 () -> assertThat(userUpdateRequest).isNotNull(),
