0b6631a54c6a87e4dc7d9826ceff6e292e95ea94
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-08-31 13:19:28 +0900

    [#43] feat: Bio vo 추가

---
+++ src/main/java/com/study/realworld/user/domain/Bio.java
@@ -0,0 +1,42 @@
+package com.study.realworld.user.domain;
+
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Bio {
+
+    @Column(name = "bio")
+    private String bio;
+
+    protected Bio() {
+    }
+
+    public Bio(String bio) {
+        this.bio = bio;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Bio bio1 = (Bio) o;
+        return Objects.equals(bio, bio1.bio);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bio);
+    }
+
+    @Override
+    public String toString() {
+        return bio;
+    }
+
+}
