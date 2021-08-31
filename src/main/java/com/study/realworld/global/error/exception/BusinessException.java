e1f6c9c477d005733f069134ec93ceb030c22b71
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-08-31 16:57:42 +0900

    [#43] feat: BusinessException 생성

---
+++ src/main/java/com/study/realworld/global/error/exception/BusinessException.java
@@ -0,0 +1,20 @@
+package com.study.realworld.global.error.exception;
+
+public class BusinessException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public BusinessException(String message, ErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public BusinessException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
+}
