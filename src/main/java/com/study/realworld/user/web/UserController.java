ab7372873005aaeab373de244e59b6e0599d83ab
Hoony <44036052+hoony-lab@users.noreply.github.com>
2021-09-05 15:16:40 +0900

    [#44] feat: add updateUser in UserController
    use `@AuthenticationPrincipal` String principal to retrieve current login user info

--- src/main/java/com/study/realworld/user/web/UserController.java
+++ src/main/java/com/study/realworld/user/web/UserController.java
@@ -5,16 +5,13 @@ import com.study.realworld.user.domain.User;
 import com.study.realworld.user.dto.UserJoinRequest;
 import com.study.realworld.user.dto.UserLoginRequest;
 import com.study.realworld.user.dto.UserResponse;
+import com.study.realworld.user.dto.UserUpdateRequest;
 import com.study.realworld.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -40,15 +37,23 @@ public class UserController {
         String token = jwtProvider.generateJwtToken(user);
 
         return ResponseEntity.ok()
-                .header(HttpHeaders.AUTHORIZATION, "Token " + token)
                 .body(UserResponse.of(user, token));
     }
 
     @GetMapping("/user")
-    public ResponseEntity<UserResponse> getUser(HttpServletRequest servletRequest) {
-        String email = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        User user = userService.findByEmail(email);
-        String token = servletRequest.getHeader(HttpHeaders.AUTHORIZATION).split(" ")[1];
+    public ResponseEntity<UserResponse> getUser(@AuthenticationPrincipal String principal) {
+        User user = userService.findByEmail(principal);
+        String token = jwtProvider.generateJwtToken(user);
+
+        return ResponseEntity.ok()
+                .body(UserResponse.of(user, token));
+    }
+
+    @PutMapping("/user")
+    public ResponseEntity<UserResponse> updateUser(@AuthenticationPrincipal String principal,
+                                                   @RequestBody UserUpdateRequest request) {
+        User user = userService.update(request, principal);
+        String token = jwtProvider.generateJwtToken(user);
 
         return ResponseEntity.ok()
                 .body(UserResponse.of(user, token));
