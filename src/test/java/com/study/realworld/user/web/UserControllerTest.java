6bdaa5dae162e8c6a7f7d9f0b83330f37e25ebb6
Hoony <44036052+hoony-lab@users.noreply.github.com>
2021-09-05 15:21:55 +0900

    [#44] test: add PUT_api_user_updateUser in UserControllerTest
    `SecurityContextHolder.getContext().setAuthentication(new Authentication());`
    
    some refactoring

--- src/test/java/com/study/realworld/user/web/UserControllerTest.java
+++ src/test/java/com/study/realworld/user/web/UserControllerTest.java
@@ -6,6 +6,7 @@ import com.study.realworld.user.domain.User;
 import com.study.realworld.user.dto.UserJoinRequest;
 import com.study.realworld.user.dto.UserLoginRequest;
 import com.study.realworld.user.dto.UserResponse;
+import com.study.realworld.user.dto.UserUpdateRequest;
 import com.study.realworld.user.service.UserService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.web.servlet.MockMvc;
@@ -29,13 +31,12 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = UserController.class
-//        includeFilters = @ComponentScan.Filter(
+//        excludeFilters = @ComponentScan.Filter(
 //                type = FilterType.REGEX, pattern = "com.study.realworld.config.auth.*")
 )
 class UserControllerTest {
@@ -50,6 +51,9 @@ class UserControllerTest {
 
     @MockBean
     private SecurityContext securityContext;
+
+    @MockBean
+    private Authentication authentication;
 
     @MockBean
     private JwtProvider jwtProvider;
@@ -68,11 +72,11 @@ class UserControllerTest {
         mvc = MockMvcBuilders
                 .webAppContextSetup(context)
                 .build();
+        SecurityContextHolder.clearContext();
     }
 
     @Test
     void POST_api_users_joinUser() throws Exception {
-
         // given
         final String URL = "/api/users";
 
@@ -98,17 +102,17 @@ class UserControllerTest {
                 .image(IMAGE)
                 .build();
 
+        // when
         when(userService.save(any()))
                 .thenReturn(user);
 
-        // when
+        // https://www.infoworld.com/article/3543268/junit-5-tutorial-part-2-unit-testing-spring-mvc-with-junit-5.html?page=3
+        // then
         ResultActions result = mvc.perform(post(URL)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(new ObjectMapper().writeValueAsString(request)))
                 .andDo(print());
 
-        // https://www.infoworld.com/article/3543268/junit-5-tutorial-part-2-unit-testing-spring-mvc-with-junit-5.html?page=3
-        // then
         verify(userService).save(any());
         result
                 .andExpect(status().isOk())
@@ -121,7 +125,6 @@ class UserControllerTest {
 
     @Test
     void POST_api_users_login_loginUser() throws Exception {
-
         // given
         final String URL = "/api/users/login";
 
@@ -138,18 +141,18 @@ class UserControllerTest {
                 .image(IMAGE)
                 .build();
 
+        // when
         when(userService.login(any()))
                 .thenReturn(user);
         when(jwtProvider.generateJwtToken(any()))
                 .thenReturn(TOKEN);
 
-        // when
+        // then
         ResultActions result = mvc.perform(post(URL)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(new ObjectMapper().writeValueAsString(request)))
                 .andDo(print());
 
-        // then
         verify(userService).login(any());
         verify(jwtProvider).generateJwtToken(any());
         result
@@ -164,7 +167,6 @@ class UserControllerTest {
 
     @Test
     void GET_api_user_getUser() throws Exception {
-
         // given
         final String URL = "/api/user";
 
@@ -176,24 +178,23 @@ class UserControllerTest {
                 .image(IMAGE)
                 .build();
 
-        final UsernamePasswordAuthenticationToken authentication =
-                new UsernamePasswordAuthenticationToken(EMAIL, TOKEN, Collections.emptyList());
-
-        when(securityContext.getAuthentication())
-                .thenReturn(authentication);
-        SecurityContextHolder.setContext(securityContext);
-
-        when(userService.findByEmail(anyString()))
-                .thenReturn(user);
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken(EMAIL, TOKEN, Collections.emptyList()));
 
         // when
+        when(userService.findByEmail(anyString()))
+                .thenReturn(user);
+        when(jwtProvider.generateJwtToken(any()))
+                .thenReturn(TOKEN);
+
+        // then
         ResultActions result = mvc.perform(get(URL)
                 .contentType(MediaType.APPLICATION_JSON)
                 .header(HttpHeaders.AUTHORIZATION, "Token " + TOKEN))
                 .andDo(print());
 
-        // then
         verify(userService).findByEmail(anyString());
+        verify(jwtProvider).generateJwtToken(any());
         result
                 .andExpect(status().isOk())
                 .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
@@ -202,6 +203,53 @@ class UserControllerTest {
                 .andExpect(jsonPath("$.user.username", is(USERNAME)))
                 .andExpect(jsonPath("$.user.bio", is(BIO)))
                 .andExpect(jsonPath("$.user.image", is(IMAGE)));
+    }
+
+    @Test
+    void PUT_api_user_updateUser() throws Exception {
+        // given
+        final String URL = "/api/user";
+
+        final UserUpdateRequest request = UserUpdateRequest.builder()
+                .email(EMAIL+EMAIL)
+                .bio(BIO+BIO)
+                .build();
+
+        final User user = User.builder()
+                .email(EMAIL+EMAIL)
+                .username(USERNAME)
+                .password(PASSWORD)
+                .bio(BIO+BIO)
+                .image(IMAGE)
+                .build();
+
+        SecurityContextHolder.getContext().setAuthentication(
+                new UsernamePasswordAuthenticationToken(EMAIL, TOKEN, Collections.emptyList()));
+
+        // when
+        when(userService.update(any(), any()))
+                .thenReturn(user);
+        when(jwtProvider.generateJwtToken(any()))
+                .thenReturn(TOKEN);
+
+        // then
+        ResultActions result = mvc.perform(put(URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(request))
+                .header(HttpHeaders.AUTHORIZATION, "Token " + TOKEN))
+                .andDo(print());
+
+        verify(userService).update(any(), any());
+        verify(jwtProvider).generateJwtToken(any());
+        result
+                .andExpect(status().isOk())
+                .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.user.email", is(request.getEmail())))
+                .andExpect(jsonPath("$.user.token", is(TOKEN)))
+                .andExpect(jsonPath("$.user.username", is(USERNAME)))
+                .andExpect(jsonPath("$.user.bio", is(request.getBio())))
+                .andExpect(jsonPath("$.user.image", is(IMAGE)));
+
     }
 
 }
