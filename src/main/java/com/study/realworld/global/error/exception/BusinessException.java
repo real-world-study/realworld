6be00ff10ab23507b69e50f57bad3a8f1a89dc87
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-06 15:15:34 +0900

    [#43] feat: BusinessException 수정 및 NotFoundException 추가

--- src/main/java/com/study/realworld/global/error/exception/BusinessException.java
+++ src/main/java/com/study/realworld/global/error/exception/BusinessException.java
@@ -2,19 +2,8 @@ package com.study.realworld.global.error.exception;
 
 public class BusinessException extends RuntimeException {
 
-    private final ErrorCode errorCode;
-
-    public BusinessException(String message, ErrorCode errorCode) {
+    public BusinessException(String message) {
         super(message);
-        this.errorCode = errorCode;
-    }
-
-    public BusinessException(ErrorCode errorCode) {
-        this.errorCode = errorCode;
-    }
-
-    public ErrorCode getErrorCode() {
-        return errorCode;
     }
 
 }
---
+++ src/main/java/com/study/realworld/global/error/exception/EntityNotFoundException.java
@@ -0,0 +1,9 @@
+package com.study.realworld.global.error.exception;
+
+public class EntityNotFoundException extends BusinessException {
+
+    public EntityNotFoundException(String message) {
+        super(message);
+    }
+
+}
