4ee881de7a62f5bd23cb31ac33961ca1a0cccc88
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-09-07 23:57:25 +0900

    [#43] refactor

--- src/main/java/com/study/realworld/global/exception/BusinessException.java
+++ src/main/java/com/study/realworld/global/exception/BusinessException.java
@@ -4,11 +4,6 @@ public class BusinessException extends RuntimeException {
 
     private ErrorCode errorCode;
 
-    public BusinessException(String message, ErrorCode errorCode) {
-        super(message);
-        this.errorCode = errorCode;
-    }
-
     public BusinessException(ErrorCode errorCode) {
         super(errorCode.getMessage());
         this.errorCode = errorCode;
@@ -17,4 +12,5 @@ public class BusinessException extends RuntimeException {
     public ErrorCode getErrorCode() {
         return errorCode;
     }
+
 }
