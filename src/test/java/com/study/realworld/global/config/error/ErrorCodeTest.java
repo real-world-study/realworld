9433950d9fa2be64d410ff479154294841ab504e
김우재 <kwj1270@naver.com>
2021-09-12 21:01:20 +0900

    feat: ErrorCode 인터페이스 생성

---
+++ src/test/java/com/study/realworld/global/config/error/ErrorCodeTest.java
@@ -0,0 +1,23 @@
+package com.study.realworld.global.config.error;
+
+import com.study.realworld.global.config.error.testUtils.TestErrorCode;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ErrorCodeTest {
+
+    @DisplayName("ErrorCode 구현채 getter 태스트")
+    @Test
+    void getter_test() {
+        final ErrorCode errorCode = TestErrorCode.TEST;
+
+        assertAll(
+                () -> assertThat(errorCode.httpStatus()).isEqualTo(HttpStatus.BAD_REQUEST),
+                () -> assertThat(errorCode.message()).isEqualTo("test message")
+        );
+    }
+}
