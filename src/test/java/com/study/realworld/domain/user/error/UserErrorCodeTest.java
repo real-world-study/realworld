6f1451066af6213b0fdfb44e62bbc26418610209
김우재 <kwj1270@naver.com>
2021-09-12 21:15:48 +0900

    teat: UserErrorCode 열거형 httpStatus 테스트 작성

--- src/test/java/com/study/realworld/domain/user/error/UserErrorCodeTest.java
+++ src/test/java/com/study/realworld/domain/user/error/UserErrorCodeTest.java
@@ -2,6 +2,7 @@ package com.study.realworld.domain.user.error;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,7 +12,14 @@ class UserErrorCodeTest {
     @DisplayName("UserErrorCode 열거형 생성 테스트")
     @Test
     void enum_create_test() {
-        assertThat(UserErrorCode.EMAIL_DUPLICATION).isNotNull();
+        final UserErrorCode emailDuplication = UserErrorCode.EMAIL_DUPLICATION;
+        assertThat(emailDuplication).isNotNull();
+    }
+
+    @DisplayName("UserErrorCode 열거형 httpStatus 테스트")
+    @Test
+    void httpStatus_test() {
+        assertThat(UserErrorCode.EMAIL_DUPLICATION.httpStatus()).isEqualTo(HttpStatus.BAD_REQUEST);
     }
 
 }
