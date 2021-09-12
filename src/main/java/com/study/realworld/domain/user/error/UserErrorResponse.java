2f2bca01c21f1a01b4442f4d01cd04ab9ba62535
김우재 <kwj1270@naver.com>
2021-09-12 21:31:44 +0900

    feat: UserErrorResponse 인스턴스 생성자 구현

--- src/main/java/com/study/realworld/domain/user/error/UserErrorResponse.java
+++ src/main/java/com/study/realworld/domain/user/error/UserErrorResponse.java
@@ -1,4 +1,38 @@
 package com.study.realworld.domain.user.error;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+import com.study.realworld.global.config.error.ErrorCode;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+import java.util.Objects;
+
+@JsonTypeName("errors")
+@JsonTypeInfo(include = JsonTypeInfo.As.WRAPPER_OBJECT, use = JsonTypeInfo.Id.NAME)
 public class UserErrorResponse {
+
+    @JsonIgnore
+    private HttpStatus httpStatus;
+
+    @JsonProperty("body")
+    private List<String> body;
+
+    UserErrorResponse() {
+    }
+
+    public UserErrorResponse(final ErrorCode errorCode) {
+        validateNull(errorCode);
+        httpStatus = errorCode.httpStatus();
+        body = List.of(errorCode.message());
+    }
+
+    private void validateNull(final ErrorCode errorCode) {
+        if(Objects.isNull(errorCode)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 }
