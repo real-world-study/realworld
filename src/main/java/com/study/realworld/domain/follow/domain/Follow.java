59d21d009a0e05a80c0a9821242c492200098923
김우재 <kwj1270@naver.com>
2021-10-03 19:12:15 +0900

    [#54] test: Follow 인스턴스 getter 테스트 작성

--- src/main/java/com/study/realworld/domain/follow/domain/Follow.java
+++ src/main/java/com/study/realworld/domain/follow/domain/Follow.java
@@ -2,12 +2,44 @@ package com.study.realworld.domain.follow.domain;
 
 import com.study.realworld.domain.user.domain.User;
 
+import javax.persistence.*;
+
+@Entity
 public class Follow {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "follow_id", updatable = false)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "following_id")
+    @Column(name = "follow_following", nullable = false, updatable = false)
     private User following;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "follower_id")
+    @Column(name = "follow_follower", nullable = false, updatable = false)
     private User follower;
 
     protected Follow() {
+    }
+
+    public Follow changeFollowing(final User following) {
+        this.following = following;
+        return this;
+    }
+
+    public Follow changeFollower(final User follower) {
+        this.follower = follower;
+        return this;
+    }
+
+    public User following() {
+        return following;
+    }
+
+    public User follower() {
+        return follower;
     }
 
     private Follow(final FollowBuilder followBuilder) {
--- src/test/java/com/study/realworld/domain/follow/domain/FollowTest.java
+++ src/test/java/com/study/realworld/domain/follow/domain/FollowTest.java
@@ -13,7 +13,7 @@ import static com.study.realworld.domain.user.domain.UserTest.userBuilder;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-class FollowTest {
+public class FollowTest {
 
     @DisplayName("Follow 인스턴스 기본 생성자 테스트")
     @Test
@@ -29,18 +29,36 @@ class FollowTest {
     @DisplayName("Follow 인스턴스 빌더 테스트")
     @Test
     void builder_test() {
-        final User user1 = userBuilder(new Email(EMAIL), new Name(USERNAME), new Password(PASSWORD), new Bio(BIO), new Image(IMAGE));
-        final User user2 = userBuilder(new Email("Email2@email.com"), new Name("differentUserName"), new Password("Password2"), new Bio("Bio2"), new Image("Image2"));
+        final User following = userBuilder(new Email(EMAIL), new Name(USERNAME), new Password(PASSWORD), new Bio(BIO), new Image(IMAGE));
+        final User follower = userBuilder(new Email("Email2@email.com"), new Name("differentUserName"), new Password("Password2"), new Bio("Bio2"), new Image("Image2"));
 
-        final Follow follow = Follow.Builder()
-                .following(user1)
-                .follower(user2)
-                .build();
+        final Follow follow = followBuilder(following, follower);
 
         assertAll(
                 () -> assertThat(follow).isNotNull(),
                 () -> assertThat(follow).isExactlyInstanceOf(Follow.class)
         );
+    }
+
+    @DisplayName("Follow 인스턴스 getter 테스트")
+    @Test
+    void getter_test() {
+        final User following = userBuilder(new Email(EMAIL), new Name(USERNAME), new Password(PASSWORD), new Bio(BIO), new Image(IMAGE));
+        final User follower = userBuilder(new Email("Email2@email.com"), new Name("differentUserName"), new Password("Password2"), new Bio("Bio2"), new Image("Image2"));
+
+        final Follow follow = followBuilder(following, follower);
+
+        assertAll(
+                () -> assertThat(follow.following()).isEqualTo(following),
+                () -> assertThat(follow.follower()).isEqualTo(follower)
+        );
+    }
+
+    public static final Follow followBuilder(final User following, final User follower) {
+        return Follow.Builder()
+                .following(following)
+                .follower(follower)
+                .build();
     }
 
 }
