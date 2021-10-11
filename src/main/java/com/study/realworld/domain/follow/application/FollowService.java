cd498fa5840f594ee922be697dc02ac08b736059
김우재 <kwj1270@naver.com>
2021-10-11 22:49:47 +0900

    [#54] feat: FollowService 인스턴스 생성자 구현

--- src/main/java/com/study/realworld/domain/follow/application/FollowService.java
+++ src/main/java/com/study/realworld/domain/follow/application/FollowService.java
@@ -1,10 +1,18 @@
 package com.study.realworld.domain.follow.application;
 
+import com.study.realworld.domain.follow.domain.FollowQueryDslRepository;
+import com.study.realworld.domain.follow.domain.FollowRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FollowService {
 
+    private final FollowRepository followRepository;
+    private final FollowQueryDslRepository followQueryDslRepository;
 
+    public FollowService(final FollowRepository followRepository, final FollowQueryDslRepository followQueryDslRepository) {
+        this.followRepository = followRepository;
+        this.followQueryDslRepository = followQueryDslRepository;
+    }
 
 }
