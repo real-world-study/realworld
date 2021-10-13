49396d9f33123aff9ade2ea7ed3a4af1ea7edd12
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-10-13 19:44:10 +0900

    [#58] feat: slug title 파라메터 생성기능 구현 및 테스트

--- src/main/java/com/study/realworld/article/domain/Slug.java
+++ src/main/java/com/study/realworld/article/domain/Slug.java
@@ -21,6 +21,10 @@ public class Slug {
         return new Slug(slug);
     }
 
+    public static Slug of(Title title) {
+        return of(title.titleToSlug());
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
--- src/test/java/com/study/realworld/article/domain/SlugTest.java
+++ src/test/java/com/study/realworld/article/domain/SlugTest.java
@@ -13,6 +13,23 @@ class SlugTest {
     }
 
     @Test
+    @DisplayName("Title 객체를 받아서 Slug 객체를 생성할 수 있다.")
+    void createSlugByTitleTest() {
+
+        // given
+        String input = "How to train% your   dragon!test";
+        Title title = Title.of(input);
+
+        Slug expected = Slug.of("how-to-train-your-dragon-test");
+
+        // when
+        Slug result = Slug.of(title);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
     @DisplayName("equals hashCode 테스트")
     void slugEqualsHashCodeTest() {
 
