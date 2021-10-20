3998981b28fd6eb99a2227b5c79c5c7f2cd68658
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-10-20 11:00:30 +0900

    [#58] feat: 이미 존재하는 tag 영속성 refresh 기능 추가 및 테스트 추가

--- src/main/java/com/study/realworld/tag/service/TagService.java
+++ src/main/java/com/study/realworld/tag/service/TagService.java
@@ -3,6 +3,8 @@ package com.study.realworld.tag.service;
 import com.study.realworld.tag.domain.Tag;
 import com.study.realworld.tag.domain.TagRepository;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +20,17 @@ public class TagService {
     @Transactional(readOnly = true)
     public List<Tag> findAll() {
         return tagRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public List<Tag> refreshTagByExistedTag(List<Tag> tags) {
+        return tags.stream()
+            .map(tag -> findByTag(tag).orElse(tag))
+            .collect(Collectors.toList());
+    }
+
+    private Optional<Tag> findByTag(Tag tag) {
+        return tagRepository.findByName(tag.name());
     }
 
 }
