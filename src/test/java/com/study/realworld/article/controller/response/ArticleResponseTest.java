33754a483be4c730e52d8e4d06dfeb3c5e899e34
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-10-20 10:13:54 +0900

    [#58] test: ArticleResponse 테스트코드 추가

---
+++ src/test/java/com/study/realworld/article/controller/response/ArticleResponseTest.java
@@ -0,0 +1,18 @@
+package com.study.realworld.article.controller.response;
+
+import com.study.realworld.article.controller.response.ArticleResponse.AuthorProfile;
+import org.junit.jupiter.api.Test;
+
+class ArticleResponseTest {
+
+    @Test
+    void articleResponseTest() {
+        ArticleResponse articleResponse = new ArticleResponse();
+    }
+
+    @Test
+    void authorProfileTest() {
+        AuthorProfile authorProfile = new AuthorProfile();
+    }
+
+}
