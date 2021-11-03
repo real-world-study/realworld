adddad93233d5a41c64db35df0f7f791460dec51
jinyoungchoi95 <jinyoungchoi95@gmail.com>
2021-11-03 23:08:11 +0900

    [#72] feat: ArticleFavorite Article vo 구현 및 테스트

---
+++ src/main/java/com/study/realworld/article/domain/FavoritingUsers.java
@@ -0,0 +1,49 @@
+package com.study.realworld.article.domain;
+
+import com.study.realworld.articlefavorite.domain.ArticleFavorite;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import javax.persistence.Embeddable;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+
+@Embeddable
+public class FavoritingUsers {
+
+    @OneToMany(mappedBy = "article", fetch = FetchType.LAZY)
+    private Set<ArticleFavorite> favorites = new HashSet<>();
+
+    protected FavoritingUsers() {
+    }
+
+    private FavoritingUsers(Set<ArticleFavorite> favorites) {
+        this.favorites = favorites;
+    }
+
+    public static FavoritingUsers of(Set<ArticleFavorite> favorites) {
+        return new FavoritingUsers(favorites);
+    }
+
+    public int size() {
+        return favorites.size();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        FavoritingUsers that = (FavoritingUsers) o;
+        return Objects.equals(favorites, that.favorites);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(favorites);
+    }
+
+}
