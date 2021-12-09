acae0fdd28017836521f97e048dc352b03e446a6
김우재 <kwj1270@naver.com>
2021-12-09 22:44:16 +0900

    test: FavoriteAcceptanceTest, 게시글에 좋아요를 취소한다

--- src/test/java/com/study/realworld/acceptance/FavoriteAcceptanceTest.java
+++ src/test/java/com/study/realworld/acceptance/FavoriteAcceptanceTest.java
@@ -4,6 +4,7 @@ import com.study.realworld.domain.article.domain.vo.ArticleSlug;
 import com.study.realworld.domain.article.dto.ArticleSave;
 import com.study.realworld.domain.auth.dto.Login;
 import com.study.realworld.domain.favorite.dto.FavoriteInfo;
+import com.study.realworld.domain.favorite.dto.UnFavoriteInfo;
 import com.study.realworld.domain.user.dto.UserJoin;
 import com.study.realworld.global.common.AccessToken;
 import io.restassured.RestAssured;
@@ -67,11 +68,52 @@ public class FavoriteAcceptanceTest extends AcceptanceTest {
         );
     }
 
+    @Test
+    void 게시글에_좋아요를_취소한다() {
+        final Login.Response user1 = 로그인_되어있음(this.user1.userEmail().userEmail());
+        final ArticleSave.Response articleSaveResponse = articleAcceptanceTest.정상적인_게시글_등록되어_있음(user1.accessToken());
+        final Login.Response user2 = 로그인_되어있음(this.user2.userEmail().userEmail());
+        final FavoriteInfo favoriteInfo = 좋아요_되어있음(user2.accessToken(), articleSaveResponse.articleSlug());
+        final ExtractableResponse<Response> response = 좋아요_취소_요청(user2.accessToken(), favoriteInfo.articleSlug());
+        final UnFavoriteInfo unFavoriteResponse = response.as(UnFavoriteInfo.class);
+
+        assertAll(
+                () -> assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(unFavoriteResponse.articleSlug()).isEqualTo(articleSaveResponse.articleSlug()),
+                () -> assertThat(unFavoriteResponse.articleTitle()).isEqualTo(articleSaveResponse.articleTitle()),
+                () -> assertThat(unFavoriteResponse.articleBody()).isEqualTo(articleSaveResponse.articleBody()),
+                () -> assertThat(unFavoriteResponse.articleDescription()).isEqualTo(articleSaveResponse.articleDescription()),
+                () -> assertThat(unFavoriteResponse.favorited()).isFalse(),
+                () -> assertThat(unFavoriteResponse.favoritesCount()).isEqualTo(0),
+                () -> assertThat(unFavoriteResponse.tagNames()).isEqualTo(Set.of()),
+                () -> assertThat(unFavoriteResponse.createdAt()).isNotNull(),
+                () -> assertThat(unFavoriteResponse.updatedAt()).isNotNull(),
+                () -> assertThat(unFavoriteResponse.authorInfo().userName()).isEqualTo(articleSaveResponse.author().userName()),
+                () -> assertThat(unFavoriteResponse.authorInfo().userBio()).isEqualTo(articleSaveResponse.author().userBio()),
+                () -> assertThat(unFavoriteResponse.authorInfo().userImage()).isEqualTo(articleSaveResponse.author().userImage()),
+                () -> assertThat(unFavoriteResponse.authorInfo().following()).isFalse()
+        );
+    }
+
+    protected FavoriteInfo 좋아요_되어있음(final AccessToken accessToken, final ArticleSlug articleSlug) {
+        final ExtractableResponse<Response> response = 좋아요_요청(accessToken, articleSlug);
+        return response.as(FavoriteInfo.class);
+    }
+
     protected ExtractableResponse<Response> 좋아요_요청(final AccessToken accessToken, final ArticleSlug articleSlug) {
         return RestAssured.given()
                 .header(AUTHORIZATION, BEARER + accessToken.accessToken())
                 .when()
                 .post(String.format("/api/articles/%s/favorite", articleSlug.articleSlug()))
+                .then()
+                .extract();
+    }
+
+    protected ExtractableResponse<Response> 좋아요_취소_요청(final AccessToken accessToken, final ArticleSlug articleSlug) {
+        return RestAssured.given()
+                .header(AUTHORIZATION, BEARER + accessToken.accessToken())
+                .when()
+                .delete(String.format("/api/articles/%s/favorite", articleSlug.articleSlug()))
                 .then()
                 .extract();
     }
